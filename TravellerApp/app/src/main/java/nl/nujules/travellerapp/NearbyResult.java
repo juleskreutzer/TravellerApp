8f44c2f3d44b99296118bd22c4d4876e7b4822d1
Jules Kreutzer <juleskreutzer@me.com>
2016-04-15 10:48:49 +0200

    Nearby Finished

--- TravellerApp/app/src/main/java/nl/nujules/travellerapp/NearbyResult.java
+++ TravellerApp/app/src/main/java/nl/nujules/travellerapp/NearbyResult.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import nl.nujules.travellerapp.util.ActivityType;
 
@@ -21,8 +22,45 @@ public class NearbyResult extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(NearbyResult.this, Menu.class);
-                intent.putExtra("activity", ActivityType.NEARBY_RESULT);
+                intent.putExtra("activity", ActivityType.NEARBY);
 
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageButton1 = (ImageButton) findViewById(R.id.btnHotelDux1);
+        ImageButton imageButton2 = (ImageButton) findViewById(R.id.btnHotelDux2);
+        ImageButton imageButton3 = (ImageButton) findViewById(R.id.btnHotelDux3);
+        ImageButton imageButton4 = (ImageButton) findViewById(R.id.btnHotelDux4);
+
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NearbyResult.this, NearbyResultSpecific.class);
+                startActivity(intent);
+            }
+        });
+
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NearbyResult.this, NearbyResultSpecific.class);
+                startActivity(intent);
+            }
+        });
+
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NearbyResult.this, NearbyResultSpecific.class);
+                startActivity(intent);
+            }
+        });
+
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NearbyResult.this, NearbyResultSpecific.class);
                 startActivity(intent);
             }
         });
---
+++ TravellerApp/app/src/main/java/nl/nujules/travellerapp/NearbyResultSpecific.java
@@ -0,0 +1,31 @@
+package nl.nujules.travellerapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import nl.nujules.travellerapp.util.ActivityType;
+
+public class NearbyResultSpecific extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby_result_specific);
+
+        Button menuButton = (Button) findViewById(R.id.btnMenu);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Replace "Placeholder.this" with the class name you implement this is.
+                Intent intent = new Intent(NearbyResultSpecific.this, Menu.class);
+                intent.putExtra("activity", ActivityType.NEARBY);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
